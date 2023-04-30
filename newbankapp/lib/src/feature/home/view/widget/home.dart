89fd172f96b4fe3f0ab9387b6115c97d1183217f
Diego Vieira <200.vieira.diego@gmail.com>
2023-04-30 15:42:23 -0300

    Página home refatorada

--- newbankapp/lib/src/feature/home/view/widget/home.dart
+++ newbankapp/lib/src/feature/home/view/widget/home.dart
@@ -19,9 +19,6 @@ class _UserHomeState extends State<UserHome> {
 
   @override
   Widget build(BuildContext context) {
-    double userBalance = 0;
-    String showedValue = userBalance.toStringAsFixed(2);
-    String hiddenValue = "••••";
     String showedVaultValue = (value).toStringAsFixed(2);
     return Scaffold(
       appBar: _newbankBar("app_name".i18n()),
@@ -29,181 +26,11 @@ class _UserHomeState extends State<UserHome> {
         scrollDirection: Axis.vertical,
         children: [
           // CARD DE SALDO EM CONTA CORRENTE
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: CardContainer(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(
-                                "Saldo em conta corrente: ",
-                                style: TextStyle(
-                                  // fontFamily: "Cormorant",
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 24,
-                                  overflow: TextOverflow.clip,
-                                ),
-                              ),
-                            ),
-                          ),
-                          ValueListenableBuilder(
-                            valueListenable: eyeListener,
-                            builder: (BuildContext context, bool selected, _) =>
-                                IconButton(
-                              iconSize: 36,
-                              onPressed: () {
-                                setState(() {
-                                  eyeListener.value = !eyeListener.value;
-                                });
-                              },
-                              icon: AnimatedSwitcher(
-                                duration: const Duration(milliseconds: 300),
-                                child: selected
-                                    ? const Icon(Icons.remove_red_eye,
-                                        key: ValueKey("iconA"))
-                                    : const Icon(Icons.remove_red_eye_outlined,
-                                        key: ValueKey("iconB")),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    AnimatedCrossFade(
-                      firstChild: Text(
-                        "R\$$showedValue",
-                        style: const TextStyle(
-                          // fontFamily: "Cormorant",
-                          fontWeight: FontWeight.bold,
-                          fontSize: 32,
-                        ),
-                      ),
-                      secondChild: Text(
-                        hiddenValue,
-                        style: const TextStyle(
-                          // fontFamily: "Cormorant",
-                          fontWeight: FontWeight.bold,
-                          fontSize: 42,
-                        ),
-                      ),
-                      crossFadeState: eyeListener.value
-                          ? CrossFadeState.showFirst
-                          : CrossFadeState.showSecond,
-                      duration: const Duration(milliseconds: 300),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
+          _accountBalance(),
           //CARD DE VALOR GUARDADO
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: CardContainer(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        "Valor Guardado Atual:",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 24,
-                        ),
-                      ),
-                    ),
-                    Text(
-                      "R\$ $showedVaultValue",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 32,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-
-          Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: CardContainer(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              "Guardar dinheiro",
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18,
-                              ),
-                            ),
-                          ),
-                          Column(
-                            children: [
-                              Padding(
-                                padding:
-                                    const EdgeInsets.only(left: 16, right: 16),
-                                child: TextFormField(
-                                  controller: valueController,
-                                  decoration: const InputDecoration(
-                                    labelStyle: TextStyle(
-                                      color: Colors.black,
-                                    ),
-                                    hintText: ("Exemplo: 42.50"),
-                                    hintStyle: TextStyle(color: Colors.black54),
-                                    labelText:
-                                        'Digite o valor que será guardado',
-                                  ),
-                                  keyboardType: TextInputType.number,
-                                  textAlign: TextAlign.center,
-                                  cursorColor: Colors.black,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  print(valueController.text);
-                  print(value);
-                  setState(() {
-                    value = double.parse(valueController.text) + value;
-                  });
-                },
-                child: Text(
-                  "Guardar",
-                ),
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.white,
-                  backgroundColor: Colors.indigo,
-                ),
-              ),
-            ],
-          ),
+          _savedValue(showedVaultValue),
+          //CARD PARA GUARDAR VALOR
+          _saveValue(),
         ],
       ),
     );
@@ -238,6 +65,191 @@ class _UserHomeState extends State<UserHome> {
         ],
       ),
       elevation: 5,
+    );
+  }
+
+  // CARD DE SALDO EM CONTA CORRENTE
+  Widget _accountBalance() {
+    double userBalance = 0;
+    String showedValue = userBalance.toStringAsFixed(2);
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: CardContainer(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    const Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          "Saldo em conta corrente: ",
+                          style: TextStyle(
+                            // fontFamily: "Cormorant",
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                            overflow: TextOverflow.clip,
+                          ),
+                        ),
+                      ),
+                    ),
+                    ValueListenableBuilder(
+                      valueListenable: eyeListener,
+                      builder: (BuildContext context, bool selected, _) =>
+                          IconButton(
+                        iconSize: 36,
+                        onPressed: () {
+                          setState(() {
+                            eyeListener.value = !eyeListener.value;
+                          });
+                        },
+                        icon: AnimatedSwitcher(
+                          duration: const Duration(milliseconds: 300),
+                          child: selected
+                              ? const Icon(Icons.remove_red_eye,
+                                  key: ValueKey("iconA"))
+                              : const Icon(Icons.remove_red_eye_outlined,
+                                  key: ValueKey("iconB")),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              AnimatedCrossFade(
+                firstChild: Text(
+                  "R\$$showedValue",
+                  style: const TextStyle(
+                    // fontFamily: "Cormorant",
+                    fontWeight: FontWeight.bold,
+                    fontSize: 32,
+                  ),
+                ),
+                secondChild: const Text(
+                  "••••",
+                  style: TextStyle(
+                    // fontFamily: "Cormorant",
+                    fontWeight: FontWeight.bold,
+                    fontSize: 42,
+                  ),
+                ),
+                crossFadeState: eyeListener.value
+                    ? CrossFadeState.showFirst
+                    : CrossFadeState.showSecond,
+                duration: const Duration(milliseconds: 300),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  //CARD DE VALOR GUARDADO
+  Widget _savedValue(String showedVaultValue) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: CardContainer(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  "Valor Guardado Atual:",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              Text(
+                "R\$ $showedVaultValue",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 32,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  //CARD PARA GUARDAR VALOR
+  Widget _saveValue() {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CardContainer(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "Guardar dinheiro",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 16, right: 16),
+                          child: TextFormField(
+                            controller: valueController,
+                            decoration: const InputDecoration(
+                              labelStyle: TextStyle(
+                                color: Colors.black,
+                              ),
+                              hintText: ("Exemplo: 42.50"),
+                              hintStyle: TextStyle(color: Colors.black54),
+                              labelText: 'Digite o valor que será guardado',
+                            ),
+                            keyboardType: TextInputType.number,
+                            textAlign: TextAlign.center,
+                            cursorColor: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            print(valueController.text);
+            print(value);
+            setState(() {
+              value = double.parse(valueController.text) + value;
+            });
+          },
+          child: Text(
+            "Guardar",
+          ),
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.indigo,
+          ),
+        ),
+      ],
     );
   }
 }
