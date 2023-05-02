57bfebc1d91386c0f4ab099e335fd60a65ccfdb4
Diego Vieira <200.vieira.diego@gmail.com>
2023-05-02 11:37:58 -0300

    Resolvido bug da pagina de signUp e adicionado botao de registrar

--- newbankapp/lib/src/feature/signup/view/page/signup.page.dart
+++ newbankapp/lib/src/feature/signup/view/page/signup.page.dart
@@ -31,7 +31,7 @@ class SignUp extends StatelessWidget {
       ),
       body: Container(
         color: Colors.white,
-        child: Column(
+        child: ListView(
           children: [
             Container(
               margin: const EdgeInsets.all(30),
@@ -40,76 +40,84 @@ class SignUp extends StatelessWidget {
                 color: Colors.indigo.withOpacity(0.8),
                 borderRadius: BorderRadius.circular(25),
               ),
-              child: Column(
-                children: [
-                  const SizedBox(
-                    height: 50,
-                  ),
-                  Text(
-                    "Sign Up",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 35,
-                        fontFamily: "Big Shoulders Display"),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Input(
-                    label: "Firstname",
-                    ctrl: firstNameTec,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Input(
-                    label: "Lastname",
-                    ctrl: lastNameTec,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Input(
-                    label: "Cpf",
-                    ctrl: cpfTec,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Input(
-                    label: "Email",
-                    ctrl: emailTec,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Input(
-                    label: "Phone",
-                    ctrl: phoneTec,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Input(
-                    label: "Username",
-                    ctrl: usernameTec,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Input(
-                    label: "Password",
-                    ctrl: passwordTec,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  // LoginButton(
-                  //   username: usernameCtrl.text,
-                  //   password: passwordCtrl.text,
-                  // )
-                ],
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Text(
+                      "Sign Up",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 35,
+                          fontFamily: "Big Shoulders Display"),
+                    ),
+
+                    Input(
+                      label: "Firstname",
+                      ctrl: firstNameTec,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Input(
+                      label: "Lastname",
+                      ctrl: lastNameTec,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Input(
+                      label: "Cpf",
+                      ctrl: cpfTec,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Input(
+                      label: "Email",
+                      ctrl: emailTec,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Input(
+                      label: "Phone",
+                      ctrl: phoneTec,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Input(
+                      label: "Username",
+                      ctrl: usernameTec,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Input(
+                      label: "Password",
+                      ctrl: passwordTec,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Registrar",
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.indigo,
+                      ),
+                    ),
+                    // LoginButton(
+                    //   username: usernameCtrl.text,
+                    //   password: passwordCtrl.text,
+                    // )
+                  ],
+                ),
               ),
             ),
           ],
