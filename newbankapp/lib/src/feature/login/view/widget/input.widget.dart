25b0a87366053fdccc6f86c4d248bfd8cebd4a36
Diego Vieira <200.vieira.diego@gmail.com>
2023-05-04 01:29:32 -0300

    adicionada input configurada

--- newbankapp/lib/src/feature/login/view/widget/input.widget.dart
+++ newbankapp/lib/src/feature/login/view/widget/input.widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class Input extends StatelessWidget {
+class Input extends StatefulWidget {
   var label = "";
   var ctrl = TextEditingController();
 
@@ -11,39 +11,92 @@ class Input extends StatelessWidget {
   });
 
   @override
+  State<Input> createState() => _InputState();
+}
+
+class _InputState extends State<Input> {
+  String text = "";
+
+  @override
   Widget build(BuildContext context) {
     return Row(
       children: <Widget>[
-        Container(
-          width: 120,
-          alignment: Alignment.centerRight,
-          child: Text(
-            label,
-            style: const TextStyle(
-                color: Colors.white,
-                fontSize: 25,
-                fontFamily: "Big Shoulders Display"),
-          ),
-        ),
-        const SizedBox(
-          width: 20,
-        ),
         Expanded(
           child: TextFormField(
-            controller: ctrl,
-            keyboardType: TextInputType.text,
-            style: const TextStyle(
+            controller: widget.ctrl,
+            keyboardType: TextInputType.name,
+            decoration: InputDecoration(
+              labelText: "${widget.label}:",
+              hintText: text,
+              labelStyle: const TextStyle(
                 color: Colors.white,
-                fontSize: 20,
-                fontFamily: "Big Shoulders Display"),
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(
-                borderSide: BorderSide(width: 1, style: BorderStyle.solid),
               ),
             ),
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontFamily: "Big Shoulders Display",
+            ),
+            onTap: () async {
+              _hintMode(widget.label);
+            },
           ),
         )
       ],
     );
+  }
+
+  void _hintMode(String text) {
+    String hintReturn = "";
+    print(text);
+    switch (text) {
+      case "Firstname":
+        {
+          hintReturn = "Ex: Jorge";
+          _updateHint(hintReturn);
+        }
+        break;
+      case "Lastname":
+        {
+          hintReturn = "Ex: Silva";
+          _updateHint(hintReturn);
+        }
+        break;
+      case "CPF":
+        {
+          hintReturn = "123.456.789-01";
+          _updateHint(hintReturn);
+        }
+        break;
+      case "Username":
+        {
+          hintReturn = "Nome123";
+          _updateHint(hintReturn);
+        }
+        break;
+      case "Password":
+        {
+          hintReturn = "Minha_senha123";
+          _updateHint(hintReturn);
+        }
+        break;
+      case "E-mail":
+        {
+          hintReturn = "example@example.com";
+          _updateHint(hintReturn);
+        }
+        break;
+      case "Phone":
+        {
+          hintReturn = "(55)14565-1234";
+          _updateHint(hintReturn);
+        }
+    }
+  }
+
+  void _updateHint(String hintReturn) {
+    setState(() {
+      text = hintReturn;
+    });
   }
 }
