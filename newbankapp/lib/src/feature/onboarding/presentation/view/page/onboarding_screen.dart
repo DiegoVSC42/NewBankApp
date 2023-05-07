7b1334a92f96203079115578c6b3d07c51a23bfc
Vinicius <deltaq3@gmail.com>
2023-05-06 23:17:17 -0300

    Algumas modificações nas telas da onboard

--- newbankapp/lib/src/feature/onboarding/presentation/view/page/onboarding_screen.dart
+++ newbankapp/lib/src/feature/onboarding/presentation/view/page/onboarding_screen.dart
@@ -19,7 +19,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   @override // Remover barra de Status do Android (transparente)
   void initState() {
     SystemChrome.setSystemUIOverlayStyle(
-        const SystemUiOverlayStyle(statusBarColor: Colors.indigo));
+        const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     super.initState();
   }
 
@@ -30,34 +30,35 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
 
   @override //Widget para aplicar o tamanho da fonte e a cor da Frase
   Widget build(BuildContext context) {
-    const bodyStyle = TextStyle(fontSize: 30.0, color: Colors.indigo);
+    const bodyStyle = TextStyle(fontSize: 28.0, color: Colors.white);
 
     const pageDecoration = PageDecoration(
       //Tamanho e cor da Fonte e o negrito do título
       titleTextStyle: TextStyle(
         fontSize: 39.0,
-        color: Colors.indigo,
+        color: Colors.white,
         fontWeight: FontWeight.w700,
+        height: 2.3,
       ),
       bodyTextStyle: bodyStyle,
       bodyPadding:
           EdgeInsets.fromLTRB(16.0, 10.0, 16.0, 16.0), // Posição da Frase
-      pageColor: Colors.white, //Cor do Fundo da página
+      pageColor: Colors.indigo, //Cor do Fundo da página
       imagePadding: EdgeInsets.zero,
     );
 
     return IntroductionScreen(
+      animationDuration: 350,
       key: introKey,
-      globalBackgroundColor: Colors.white,
+      globalBackgroundColor: Colors.indigo,
       pages: [
         // Cada página da onboard (Page View Model)
         PageViewModel(
           title: "welcome".i18n(), // Título
-
           body: "phrase_ob1".i18n(), // Frase
           image: _buildImage('imagem-1.png'), //Imagem utilizada
           decoration:
-              pageDecoration, // Uso do contrutor para cada página (Manter o mesmo padrão)
+              pageDecoration, // Uso do container para cada página (Manter o mesmo padrão)
         ),
 
         PageViewModel(
@@ -76,9 +77,9 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                   padding: EdgeInsets.all(8.0),
                   child: Text(
                     "phrase_ob3".i18n(),
-                    style: TextStyle(
-                      color: Colors.indigo,
-                      fontSize: 30,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 28,
                     ),
                   ),
                 ),
@@ -87,23 +88,25 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                 onPressed: () {
                   openAuthScreen(context);
                 },
-                // Aplica um stilo a Barra
+                // Aplica um estilo a Barra
                 style: ElevatedButton.styleFrom(
                   // Elevado
                   minimumSize:
                       const Size.fromHeight(80), //Aumenta a barra (Vertical)
-                  backgroundColor: Colors.indigo, // Muda a cor
+                  backgroundColor: Colors.white, // Muda a cor
                   //Aplica o estido de borda da barra
                   shape: RoundedRectangleBorder(
                     borderRadius:
-                        BorderRadius.circular(10.0), //Quão redondo aplica
+                        BorderRadius.circular(40.0), //Quão redondo aplica
                   ),
                 ),
-                //Quual texto colocana barra
+                //Qual texto coloca na barra
                 child: Text(
                   "phrase_ob4".i18n(),
-                  style: TextStyle(
-                      color: Colors.white, fontSize: 15.5), //Cor do texto
+                  style: const TextStyle(
+                    color: Colors.indigo,
+                    fontSize: 16.0,
+                  ), //Cor do texto
                 ),
               ),
             ],
@@ -114,31 +117,32 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
           decoration: pageDecoration,
         ),
       ],
+      //
       showBackButton: true,
       showSkipButton: false,
       showDoneButton: false,
-      //skipOrBackFlex: 0,
+
       nextFlex: 1, // Posição de quando aperta em Próximo
       // Botão de Pular
       skip: Text('skip'.i18n(),
-          style: TextStyle(fontWeight: FontWeight.w800, color: Colors.indigo)),
+          style: TextStyle(fontWeight: FontWeight.w800, color: Colors.white)),
       // Botão de Voltar
       back: Text('back'.i18n(),
-          style: TextStyle(fontWeight: FontWeight.w800, color: Colors.indigo)),
+          style: TextStyle(fontWeight: FontWeight.w800, color: Colors.white)),
       // Botão de Próximo
       next: Text('next'.i18n(),
-          style: TextStyle(fontWeight: FontWeight.w800, color: Colors.indigo)),
+          style: TextStyle(fontWeight: FontWeight.w800, color: Colors.white)),
       //
-      curve: Curves.fastLinearToSlowEaseIn, // Atraso para passar as páginas
+      curve: Curves.linear, //Estilo de passagem de páginas
       //
-      controlsMargin: const EdgeInsets.all(16), //Margem até os botões
+      controlsMargin: const EdgeInsets.all(17), //Margem até os botões
       controlsPadding: const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
       // Estilo das bolinhas
       dotsDecorator: const DotsDecorator(
         size: Size(8.0, 8.0), //Tamanho
-        color: Color.fromARGB(255, 185, 193, 240), // Cor do Fundo
-        activeSize: Size(15.0, 5.0), // Tamanho de quando está em cima
-        activeColor: Colors.indigo, // Cor de quando está em cima
+        color: Color.fromARGB(255, 194, 200, 235), // Cor do Fundo
+        activeSize: Size(12.0, 12.0), // Tamanho de quando está em cima
+        activeColor: Colors.white, // Cor de quando está em cima
         // Estilo de quando está em cima
         activeShape: RoundedRectangleBorder(
           // Estilo e quão redondo aplica nas bordas
