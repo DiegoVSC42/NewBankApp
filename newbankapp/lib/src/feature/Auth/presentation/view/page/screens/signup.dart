0a01cf2a942758819cb2bafed36ad85d49e27895
Diego Vieira <200.vieira.diego@gmail.com>
2023-06-23 16:39:59 -0300

    Corrigido bug visual signup

--- newbankapp/lib/src/feature/Auth/presentation/view/page/screens/signup.dart
+++ newbankapp/lib/src/feature/Auth/presentation/view/page/screens/signup.dart
@@ -35,13 +35,12 @@ class _SignUpState extends State<SignUp> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: NewBankBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: NewBankBoxCard(
-          boxContent: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text(
+      body: NewBankBoxCard(
+        boxContent: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
                 "sign_up".i18n(),
                 style: TextStyle(
                   fontSize: 32,
@@ -49,51 +48,75 @@ class _SignUpState extends State<SignUp> {
                 ),
                 textAlign: TextAlign.center,
               ),
-              Input(
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Input(
                 label: "firstname".i18n(),
                 ctrl: firstNameTec,
                 regex: RegExp(r'\w+'),
                 obscureText: false,
               ),
-              Input(
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Input(
                 label: "lastname".i18n(),
                 ctrl: lastNameTec,
                 regex: RegExp(r'\w+'),
                 obscureText: false,
               ),
-              Input(
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Input(
                 label: "CPF".i18n(),
                 ctrl: cpfTec,
                 regex: RegExp(r'\d{11}'),
                 obscureText: false,
               ),
-              Input(
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Input(
                 label: "email".i18n(),
                 ctrl: emailTec,
                 regex: RegExp(
                     r'(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$'),
                 obscureText: false,
               ),
-              Input(
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Input(
                 label: "phone".i18n(),
                 ctrl: phoneTec,
                 regex: RegExp(r'\d{11}'),
                 obscureText: false,
               ),
-              Input(
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Input(
                 label: "username_sup".i18n(),
                 ctrl: usernameTec,
                 regex: RegExp(r'\w{4,}'),
                 obscureText: false,
               ),
-              Input(
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Input(
                 label: "password_sup".i18n(),
                 ctrl: passwordTec,
                 regex: RegExp(
                     r'(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$'),
                 obscureText: true,
               ),
-              ElevatedButton(
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
                 onPressed: () {},
                 child: Text(
                   "sign_up_sup".i18n(),
@@ -101,8 +124,8 @@ class _SignUpState extends State<SignUp> {
                 style: ButtonStyle(
                     minimumSize: MaterialStateProperty.all(Size(320, 40))),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
